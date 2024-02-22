1d0a11eb143a10ffda632b464941fd792ff9582a
Garima Sablok <105988269+garimasablok@users.noreply.github.com>
2024-02-22 12:59:47 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+document.write('<img src="image/bg1.jpg" alt="banner" id="bg" height="250px" width="31%">')
+
+document.write('<img src="image/p1.jpg" alt="banner" id="bg1" height="150px" width="150px">')
+
+
+document.write('<h2 id="head">NAME:<br><br>Contact:<br><br>EMAIL:</h2>')
